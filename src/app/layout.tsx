90ea1bdb35ebff235d54258d8251e402e3cd2026
Pedro de Souza Goettert <pedrogoettert@hotmail.com>
2025-06-24 09:14:31 -0300

    chore: change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
 	title: "analyze pro",
 	description: "Gestão de projetos voltado para quem trabalha com tráfego pago",
+	icons: {
+		icon: "./favicon.ico",
+	},
 };
 
 export default function RootLayout({
